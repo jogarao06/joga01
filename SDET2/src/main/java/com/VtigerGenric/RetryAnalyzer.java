635d84f48977222e9d72418e4cd19733286253f1
jogarao06 <joga123123@gmail.com>
2021-12-16 13:48:47 +0530

    last1

--- SDET2/src/main/java/com/VtigerGenric/RetryAnalyzer.java
+++ SDET2/src/main/java/com/VtigerGenric/RetryAnalyzer.java
@@ -5,7 +5,7 @@ import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer{
 	int counter=0;
-	int retrylimit=4;
+	int retrylimit=2;
 
 	public boolean retry(ITestResult result) {
 		if(counter<retrylimit) {
